010562e0aac6be7bd5a122f993b99fabe0d25b54
Taump <taump@yandex.ru>
2025-05-19 17:18:34 +0200

    feat: add local

---
+++ src/local/description.ts
@@ -0,0 +1,14 @@
+interface IPageDescriptions {
+    [key: string]: string;
+}
+
+export default {
+    claim: "You became neighbors and can claim your reward house and plot — while getting to know your neighbor",
+    main_empty: "A community engagement space where Obyte community members establish closer connections with each other and receive rewards after becoming neighbors in the City",
+    main_with_unit: "{type} at {address} owned by {name} in Obyte City",
+    market: "P2P market where existing plots of land can be bought and sold",
+    governance: "Governance of Obyte City, a community engagement space for Obyte",
+    faq: "Frequently asked questions about Obyte City, a community engagement space for Obyte",
+    user: "Personal page of citizen {name}, {wallet_address} in Obyte City, a community engagement space for Obyte",
+    default: "A community engagement space where Obyte community members establish closer connections with each other and receive rewards after becoming neighbors in the City",
+} as IPageDescriptions;
